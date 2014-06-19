1f40b6380a75f99574f527a92365c7a56ed11c65
mbrown101 <m.g.brown85@gmail.com>
2014-06-18 20:23:14 -0700

    Update cachematrix.R
    more commenting by line for working code

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,15 @@
-
 ## @@ -1,8 +1,8 @@
-## This file provides functions for inverting a matrix and storing its inverted value
+## This file contains two functions intended to work togather in order to  invert 
+## a matrix and store its inverted value.
 
-## Function makeCacheMatrix creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
+## Function makeCacheMatrix creates a special "matrix" object that can cache 
+## its inverse.
+
+
+makeCacheMatrix <- function(x = matrix()) {     ## pass in a matrix
+  inv <- NULL                                   ## assign the variable inv to null
+  set <- function(y) {                          ## define the set function to assign values to x and inv in the parent environment
     x <<- y
     inv <<- NULL
   }
@@ -25,17 +28,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## (and the matrix has not changed), then the cachesolve should 
 ## retrieve the inverse from the cache.
 
-  
-cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
-  inv <- x$getinv()
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data, ...)
-  x$setinv(inv)
-  inv
 
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()                                          ## get value for the inverted matrix
+  if(!is.null(inv)) {                                        ## check to see if a value exists
+    message("getting cached data")
+    return(inv)                                              ## if value, return the cached inv value
+  }
+  data <- x$get()                                            
+  inv <- solve(data, ...)                                    ## if no value for inv cached, call solve() on the input matrix
+  x$setinv(inv)                                              ## cache value of inverted matrix 
+  inv                                                        ## reference variable inv to return the inverted matrix
+  
 }
